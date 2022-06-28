a54ff89985dbb6409e84093d42fbcf979be1e92b
Yash-Kumar-19 <kyash0000@gmail.com>
2022-06-28 17:26:47 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Yash1999:3Bw0gG3jLVVmLcCb@cluster0.0pxxn.mongodb.net/group25Database", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
